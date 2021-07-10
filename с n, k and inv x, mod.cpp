e4eb7a73f7763b50db71d549ff15c65482f02fa6
Astronomax <53699562+Astronomax@users.noreply.github.com>
2021-07-10 15:09:13 +0300

    Update с n, k and inv x, mod.cpp

--- с n, k and inv x, mod.cpp
+++ с n, k and inv x, mod.cpp
@@ -1,49 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define _USE_MATH_DEFINES
-#include <iostream>
-#include <cstdio>
-#include <vector>
-#include <set>
-#include <queue>
-#include <map>
-#include <string>
-#include <cmath>
-#include <functional>
-#include <algorithm>
-#include <utility>
-#include <stack>
-#include <unordered_map>
-#include <unordered_set>
-#include <iterator>
-#include <fstream>
-#include <iomanip>
-#include <time.h>
-#include <complex>
-#include <numeric>
+#include <bits/stdc++.h>
+#include <random>
+#include <chrono>
 
 using namespace std;
 
 typedef long double C;
 typedef complex<C> P;
-
 #define X real()
 #define Y imag()
-#define Size(X) (int)X.size()
-#define int long long
-#define ui unsigned int
-#define mp make_pair
-#define timer fghge
-#define y1 yjyjyju
-#define all(X) (X).begin(), (X).end()
 #define endl '\n'
+#define int long long
+#define all(X) (X).begin(), (X).end()
+#define rall(X) (X).rbegin(), (X).rend()
+mt19937_64 rnd(chrono::system_clock::now().time_since_epoch().count());
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b) { return a * b / gcd(a, b); }
 
-const int mod = 998244353;
+const int mod = 998244353, MAXN = 510;
+int dp[MAXN][MAXN], pw[MAXN];
 
-int gcd(int a, int b) {
-	if (b == 0)
-		return a;
-	else
-		return gcd(b, a % b);
+template <typename T>
+T add(T n) {
+	int ans = n % mod;
+	if (ans < 0) ans += mod;
+	return ans;
+}
+
+template <typename T, typename... Args>
+T add(T n, Args... rest) {
+	int ans = (n + add(rest...)) % mod;
+	if (ans < 0) ans += mod;
+	return ans;
+}
+
+template <typename T>
+T prod(T n) {
+	return n % mod;
+}
+
+template <typename T, typename... Args>
+T prod(T n, Args... rest) {
+	return ((n % mod) * prod(rest...)) % mod;
 }
 
 void extgcd(int a, int b, int &x, int &y) {
@@ -66,12 +64,10 @@ int inv(int a, int mod) {
 	return x;
 }
 
-const int MAXN = 500005;
 int f[MAXN];
 
 int c(int n, int k) {
-	if (n < 0 || n < 0 || n - k < 0)
-		return 0;
+	if (n < 0 || k < 0 || n - k < 0) return 0;
 	return (f[n] * inv(f[n - k] * f[k], mod)) % mod;
 }
 
@@ -86,17 +82,11 @@ int kuznets(int n, int k, int d) {
 signed main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	//freopen("input.txt", "r", stdin);
-
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
 	f[0] = 1;
 	for (int i = 1; i < MAXN; i++)
 		f[i] = (f[i - 1] * i) % mod;
-
-	int n, k; cin >> n >> k;
-	int ans = 0;
-	for (int i = 1; i <= n; i++) 
-		if(k - 1 <= n / i - 1)
-			ans = (ans + c(n / i - 1, k - 1)) % mod;
-	cout << ans;
 	return 0;
 }
